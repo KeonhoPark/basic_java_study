f7b04b900d751eabcbbb9af460cf7bc0f0945d7a
KeonhoPark <gse04063@gmail.com>
2022-03-30 23:05:47 +0900

    문자열 연산

---
+++ src/StringOperation.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+
+public class StringOperation {
+
+    public static void main(String args[]){
+        System.out.println("hello world".length());
+        System.out.println("hello world".replace("world", "java"));
+        System.out.println("what is your name?".toUpperCase());
+        System.out.println("Keonho".equals("keonho"));
+    }
+}
