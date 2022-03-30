fdd1a8baf79c69e297421b5072748d9dae9e8860
KeonhoPark <gse04063@gmail.com>
2022-03-30 22:46:04 +0900

    number

---
+++ src/Numbers.java
@@ -0,0 +1,10 @@
+public class Numbers {
+
+    public static void main(String[] args){
+        System.out.println(6+6);
+
+        System.out.println(Math.PI);
+        System.out.println(Math.floor(Math.PI));
+        System.out.println(Math.ceil(Math.PI));
+    }
+}
