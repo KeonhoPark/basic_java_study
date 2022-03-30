35c392dd90d8a6cd1aa6214c3cf60c289e08c4c5
KeonhoPark <gse04063@gmail.com>
2022-03-30 22:56:50 +0900

    문자열, 이스케이프 기호

---
+++ src/StringApp.java
@@ -0,0 +1,13 @@
+public class StringApp {
+    public static void main(String args[]){
+        System.out.println("hello");
+        System.out.println("h");
+        System.out.println('h');
+        //System.out.println('hello');
+
+        System.out.println("hello\n");
+        System.out.println("world");
+        System.out.println("\"hello world\"");
+        System.out.println("hello\tworld");
+    }
+}
