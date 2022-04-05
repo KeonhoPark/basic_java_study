d44684aa5c2f808f25ccbf02eca048f46c7acbf6
KeonhoPark <gse04063@gmail.com>
2022-04-05 21:41:59 +0900

    3 Boolean 데이터 타입 및 비교연산자

---
+++ src/BooleanApp.java
@@ -0,0 +1,10 @@
+public class BooleanApp {
+
+    public static void main(String[] args){
+        String foo = "hello world";
+        System.out.println(foo.contains("world"));
+
+        System.out.println(1 > 1);
+        System.out.println(1 == 1);
+    }
+}
