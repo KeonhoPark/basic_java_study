13502948f6363bbe2ec463e2a43eb850403fba7b
KeonhoPark <gse04063@gmail.com>
2022-04-13 17:30:03 +0900

    강의 4 메소드의 입력

---
+++ src/FirstMethod.java
@@ -0,0 +1,12 @@
+public class FirstMethod {
+
+    public static void main(String[] args){
+
+        printTwoTimes("b");
+    }
+
+    public static void printTwoTimes(String input){
+        System.out.println(input);
+        System.out.println(input);
+    }
+}
