416bc03d3690ce85b9dac93486d285758b7fa543
KeonhoPark <gse04063@gmail.com>
2022-05-02 23:01:04 +0900

    부스트코스 자바2 오버로딩, 오버라이딩

--- src/InheritanceApp.java
+++ src/InheritanceApp.java
@@ -2,11 +2,27 @@ class Cal{
     public int sum(int v1, int v2){
         return v1 + v2;
     }
+
+    //오버로딩(이름이 같은 메소드를 형태를 다르게 정의(파라미터 값을 다르게 하여)
+    public int sum(int v1, int v2, int v3){
+        return v1 + v2 + v3;
+    }
 }
 
 class Cal2 extends Cal{
     public int minus(int v1, int v2){
         return v1 - v2;
+    }
+
+    //오버라이딩(부모 클래스의 메소드를 자식 클래스에서 재정의)
+    public int sum(int v1, int v2){
+        System.out.println("Cal2 sum");
+        return v1 + v2;
+    }
+
+    //오버로딩이자 오버라이딩
+    public int sum(int v1, int v2, int v3){
+        return v1 + v2 + v3;
     }
 }
 
