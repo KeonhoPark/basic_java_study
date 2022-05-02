ffc8bcd6408cd7c3e8a1703c8a4671cd95967458
KeonhoPark <gse04063@gmail.com>
2022-05-02 23:10:17 +0900

    부스트코스 자바2 this, super

--- src/InheritanceApp.java
+++ src/InheritanceApp.java
@@ -20,6 +20,13 @@ class Cal2 extends Cal{
         return v1 + v2;
     }
 
+    //super 키워드를 이용하여 부모 클래스의 sum 메소드 호출
+    public int sum(int v1, int v2, int v3, int v4){
+        System.out.println("super sum");
+        return super.sum(v1, v2, v3) + v4;
+    }
+
+
     //오버로딩이자 오버라이딩
     public int sum(int v1, int v2, int v3){
         return v1 + v2 + v3;
@@ -30,7 +37,7 @@ public class InheritanceApp {
 
     public static void main(String[] args) {
         Cal2 c2 = new Cal2();
-        System.out.println(c2.sum(1,2));
+        System.out.println(c2.sum(1,2,3,4));
         System.out.println(c2.minus(1,2));
     }
 }
