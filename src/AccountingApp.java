11af456c875dd0c1500bb44d590e178793dcf715
KeonhoPark <gse04063@gmail.com>
2022-05-23 22:03:53 +0900

    부스트코스 나의 앱만들기_조건문 도입

--- src/AccountingApp.java
+++ src/AccountingApp.java
@@ -4,13 +4,30 @@ public class AccountingApp {
         double valueOfSupply = 10000.0;
         double vatRate = 0.1;
         double expenseRate = 0.3;
+        double expense = valueOfSupply * expenseRate;
+        double total = valueOfSupply + (valueOfSupply * vatRate);
+        double vat = valueOfSupply * vatRate;
+        double income = valueOfSupply - expense;
+
         double dividend1 = valueOfSupply * 0.5;
         double dividend2 = valueOfSupply * 0.3;
         double dividend3 = valueOfSupply * 0.2;
+
+        if(income < 10000.0){
+            dividend1 = income * 1.0;
+            dividend2 = income * 0;
+            dividend3 = income * 0;
+        }
+        else{
+            dividend1 = income * 0.5;
+            dividend2 = income * 0.3;
+            dividend3 = income * 0.2;
+        }
         System.out.println("Value of Supply : "+ valueOfSupply);
-        System.out.println("VAT : "+(valueOfSupply * vatRate));
-        System.out.println("Total : "+ valueOfSupply + (valueOfSupply * vatRate));
-        System.out.println("Expense : "+(valueOfSupply * expenseRate));
+        System.out.println("VAT : "+ vat);
+        System.out.println("Total : "+ total);
+        System.out.println("Expense : "+expense);
+        System.out.println("Income : "+income);
         System.out.println("Dividend1 : "+dividend1);
         System.out.println("Dividend2 : "+dividend2);
         System.out.println("Dividend3 : "+dividend3);
