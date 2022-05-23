b89d20a78f848e88ba374a0cb2ed8773283ab866
KeonhoPark <gse04063@gmail.com>
2022-05-23 22:33:11 +0900

    부스트코스 나의 앱만들기_메소드 도입

--- src/AccountingArrayApp.java
+++ src/AccountingArrayApp.java
@@ -1,20 +1,34 @@
 public class AccountingArrayApp {
 
-    public static void main(String[] args) {
-        double valueOfSupply = 10000.0;
-        double vatRate = 0.1;
-        double expenseRate = 0.3;
-        double expense = valueOfSupply * expenseRate;
-        double total = valueOfSupply + (valueOfSupply * vatRate);
-        double vat = valueOfSupply * vatRate;
-        double income = valueOfSupply - expense;
+    public static double valueOfSupply;
+    public static double vatRate;
 
+    public static double getExpense(double valueOfSupply, double expenseRate) {
+        return valueOfSupply * expenseRate;
+    }
+
+    public static double getVat(double valueOfSupply, double vatRate) {
+        return valueOfSupply * vatRate;
+    }
+
+    public static double getTotal(double valueOfSupply, double vatRate) {
+        return valueOfSupply + getExpense(valueOfSupply, vatRate);
+    }
+
+    private static double getIncome(double valueOfSupply, double expense) {
+        return valueOfSupply - expense;
+    }
+
+    public static void main(String[] args) {
+        valueOfSupply = 10000.0;
+        vatRate = 0.1;
+        double expenseRate = 0.3;
 
         System.out.println("Value of Supply : "+ valueOfSupply);
-        System.out.println("VAT : "+ vat);
-        System.out.println("Total : "+ total);
-        System.out.println("Expense : "+expense);
-        System.out.println("Income : "+income);
+        System.out.println("VAT : "+ getVat(valueOfSupply, vatRate));
+        System.out.println("Total : "+ getTotal(valueOfSupply, vatRate));
+        System.out.println("Expense : "+getExpense(valueOfSupply, vatRate));
+        System.out.println("Income : "+getIncome(valueOfSupply, getExpense(valueOfSupply, expenseRate)));
 
         double[] dividendRates = new double[3];
         dividendRates[0] = 0.5;
@@ -23,7 +37,7 @@ public class AccountingArrayApp {
 
         int i = 0;
         while (i < 3){
-            System.out.println("dividend : "+income * dividendRates[i]);
+            System.out.println("dividend : "+ getExpense(getIncome(valueOfSupply, getExpense(valueOfSupply, expenseRate)), dividendRates[i]));
             i++;
         }
 
