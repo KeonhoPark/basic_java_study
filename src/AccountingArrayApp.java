886bf4468dc339f20c9ae1ee99711969d33c7042
KeonhoPark <gse04063@gmail.com>
2022-05-24 00:12:46 +0900

    부스트코스 나의 앱만들기_클래스, 인스턴스 도입

--- src/AccountingArrayApp.java
+++ src/AccountingArrayApp.java
@@ -1,45 +1,43 @@
-public class AccountingArrayApp {
+class AccountingClass {
+    public double valueOfSupply;
+    public double vatRate;
+    double expenseRate = 0.3;
 
-    public static double valueOfSupply;
-    public static double vatRate;
-
-    public static double getExpense(double valueOfSupply, double expenseRate) {
+    public double getExpense(double valueOfSupply, double expenseRate) {
         return valueOfSupply * expenseRate;
     }
 
-    public static double getVat(double valueOfSupply, double vatRate) {
+    public double getVat(double valueOfSupply, double vatRate) {
         return valueOfSupply * vatRate;
     }
 
-    public static double getTotal(double valueOfSupply, double vatRate) {
+    public double getTotal(double valueOfSupply, double vatRate) {
         return valueOfSupply + getExpense(valueOfSupply, vatRate);
     }
 
-    private static double getIncome(double valueOfSupply, double expense) {
+    public double getIncome(double valueOfSupply, double expense) {
         return valueOfSupply - expense;
     }
 
-    public static void main(String[] args) {
-        valueOfSupply = 10000.0;
-        vatRate = 0.1;
-        double expenseRate = 0.3;
-
+    public void print(){
         System.out.println("Value of Supply : "+ valueOfSupply);
         System.out.println("VAT : "+ getVat(valueOfSupply, vatRate));
         System.out.println("Total : "+ getTotal(valueOfSupply, vatRate));
         System.out.println("Expense : "+getExpense(valueOfSupply, vatRate));
         System.out.println("Income : "+getIncome(valueOfSupply, getExpense(valueOfSupply, expenseRate)));
+    }
+}
 
-        double[] dividendRates = new double[3];
-        dividendRates[0] = 0.5;
-        dividendRates[1] = 0.3;
-        dividendRates[2] = 0.2;
 
-        int i = 0;
-        while (i < 3){
-            System.out.println("dividend : "+ getExpense(getIncome(valueOfSupply, getExpense(valueOfSupply, expenseRate)), dividendRates[i]));
-            i++;
-        }
+
+public class AccountingArrayApp {
+
+
+    public static void main(String[] args) {
+
+        AccountingClass accountingClass = new AccountingClass();
+
+        accountingClass.print();
 
 
     }
